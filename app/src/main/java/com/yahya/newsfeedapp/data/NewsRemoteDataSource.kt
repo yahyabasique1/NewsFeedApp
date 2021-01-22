a415c4a5cbd149430b4625391b748567b830073f
Yahya Basique <yahya.basique@cybernetyx.com>
2021-01-22 18:04:33 +0530

    Added screenshot

--- app/src/main/java/com/yahya/newsfeedapp/data/NewsRemoteDataSource.kt
+++ app/src/main/java/com/yahya/newsfeedapp/data/NewsRemoteDataSource.kt
@@ -3,14 +3,11 @@ package com.yahya.newsfeedapp.data
 
 import com.yahya.newsfeedapp.repository.remote.*
 import javax.inject.Inject
-
+const val API_KEY="enter_your_api_key_here"
 /* Works with the News API to get data. */
 class NewsRemoteDataSource @Inject constructor(private val service: ApiInterface) : BaseDataSource() {
 
     suspend fun fetchNewsList(query:String="Cristiano Ronaldo",apiKey : String, page : Int, pageSize : Int ) : ResponseResult<NewsListResponse> {
-        return getResult { service.getTopNewsList("d2faa7e768114a1baa203c73224a3523", page,pageSize, query) }
+        return getResult { service.getTopNewsList(API_KEY, page,pageSize, query) }
     }
 }
-//f7d11217cf16472c8db908bee399e577
-//58fd5ec4008b428cb5f8860843b91140
-//d2faa7e768114a1baa203c73224a3523
